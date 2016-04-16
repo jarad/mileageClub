85515d73130b52944a05556282d2d2e1822da78e
jarad <niemi@iastate.edu>
2016-04-16 13:06:20 -0500

    Add milestones.R

---
+++ R/milestones.R
@@ -0,0 +1,24 @@
+#' Calculate milestones
+#' 
+#' This will calculate, by week, the number of students hitting a particular
+#' milestone (every 5 miles) and the cumulative number of students who have hit
+#' that milestone as of that week. 
+#' 
+#' @param d A \code{data.frame} containing all card and day information.
+#' @return A \code{data_frame} with the number of students hitting a particular milestone. 
+#' @import dplyr
+#' @export 
+#' @examples 
+#' \dontrun{
+#' read_cards() %>% milestones
+#' }
+milestones = function(d) {
+  d %>% 
+    cumulative_miles_by_id %>%
+    group_by_("cumulative_miles", "week") %>%
+    summarize_(n = n()) %>%
+    mutate_(milestone = "cumulative_miles") %>%
+    ungroup() %>%
+    select_(-"cumulative_miles") %>%
+    tidyr::spread_("week", "n", fill=0)
+}
