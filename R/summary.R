2d3ea2e022aa1c186553c40d794d411fec152df5
jarad <niemi@iastate.edu>
2016-04-16 13:27:02 -0500

    Move shiny app to summary.R

--- R/summary.R
+++ R/summary.R
@@ -3,16 +3,40 @@
 #' Get the mileage club summary as a shiny app which reports the miles run each week,
 #' the cumulative miles run, and some other information.
 #' 
+#' @param cards A \code{data.frame} contining all the cards with the weeks that they were completed.
+#' @param ids A \code{data.frame} containing the student
+#' @import shiny
 #' @export
 #' @examples
 #' \dontrun{
 #' summary()
 #' }
-summary = function() {
-  appDir = system.file("shiny-examples","mileageClub", package="mileageClub")
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `mileageClub`.", call. = FALSE)
-  }
+summary = function(cards,ids) {
+  total_miles = cumulative_miles(cards)
   
-  shiny::runApp(appDir, display.mode = "normal", launch.browser = TRUE)
+  milestones = milestones(cards) %>%
+    mutate(Total = rowSums(.) - milestone)
+  
+  shinyApp(
+    ui = shinyUI(navbarPage(
+      title = 'MGMC Mileage Club - Fellows',
+      tabPanel('Find student',     DT::dataTableOutput('ex1')),
+      tabPanel('Total Miles',      DT::dataTableOutput('total_miles')),
+      tabPanel('Milestones',       DT::dataTableOutput('milestones'))
+    )),
+    server = function(input, output) {
+      
+      output$ex1 = DT::renderDataTable(
+        DT::datatable(ids, options = list(pageLength = 25))
+      )
+      
+      output$total_miles = DT::renderDataTable(
+        DT::datatable(total_miles)
+      )
+      
+      output$milestones = DT::renderDataTable(
+        DT::datatable(milestones)
+      )
+    }
+  )
 }
