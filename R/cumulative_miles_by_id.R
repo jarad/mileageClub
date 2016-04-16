c0094a893fe3f9f88960598b7738a4060629c2dc
jarad <niemi@iastate.edu>
2016-04-16 13:05:38 -0500

    Add cumulative_mile_by_id

--- R/cumulative_miles_by_id.R
+++ R/cumulative_miles_by_id.R
@@ -6,12 +6,17 @@
 #' @param d A \code{data.frame} containing all the card and day information.
 #' @param miles_per_card An integer specifying how many miles a card is worth.
 #' @return A \code{data.frame} with the cumulative miles for each individual by week.
+#' @import dplyr
 #' @export
-cumulative_miles_by_id <-
-function(d, miles_per_card=5) {
+#' @examples 
+#' \dontrun{
+#' read_cards() %>% cumulative_miles_by_id
+#' }
+cumulative_miles_by_id = function(d, miles_per_card=5) {
   d %>% 
-    group_by(id) %>%
-    arrange(id, week) %>%
-    mutate(one = 1, cumulative_miles = miles_per_card*cumsum(one)) %>%
-    select(-one)
+    group_by_("id") %>%
+    arrange_("id", "week") %>%
+    mutate_(one = 1, 
+            cumulative_miles = miles_per_card*cumsum("one")) %>%
+    select_(-"one")
 }
